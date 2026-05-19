b6ee14dece51f7e65b7c12a1def5bfd7fe47246a
jailson Ribeiro <jailsonribeiro7@gmail.com>
2026-05-19 16:57:07 -0300

    feature:entity and resource

---
+++ src/main/java/com/jailsondev/course/resources/userResource.java
@@ -0,0 +1,18 @@
+package com.jailsondev.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jailsondev.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class userResource {
+	@GetMapping
+public ResponseEntity<User> findAll(){
+	User u = new User(1L, "Maria", "Maria@gmail.com", "(98) 98107-3034", "maria073034");
+	return ResponseEntity.ok().body(u);
+}
+}
