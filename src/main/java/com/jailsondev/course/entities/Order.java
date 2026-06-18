6c90c0a4c21ad761223b3b0a53ff6bad2b599261
jailson Ribeiro <jailsonribeiro7@gmail.com>
2026-06-17 21:58:30 -0300

    refactor:Subtotal & Total methods

--- src/main/java/com/jailsondev/course/entities/Order.java
+++ src/main/java/com/jailsondev/course/entities/Order.java
@@ -93,6 +93,11 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems(){
 		return items;
 	}
+	public Double getTotal() {
+		
+	return items.stream().mapToDouble(OrderItem::getSubTotal).reduce(0, Double::sum);
+		
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
