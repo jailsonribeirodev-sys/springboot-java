5e9dfc824a7b97cfa32d431512f73bf78d684bc8
jailson Ribeiro <jailsonribeiro7@gmail.com>
2026-06-21 18:10:44 -0300

    Exception handling - findById

--- src/main/java/com/jailsondev/course/services/UserService.java
+++ src/main/java/com/jailsondev/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.jailsondev.course.entities.User;
 import com.jailsondev.course.repositories.UserRepository;
+import com.jailsondev.course.services.exceptions.ResourceNotFoundException;
 @Service
 public class UserService {
 	@Autowired
@@ -18,7 +19,7 @@ public class UserService {
 	}
 	public User findById(Long id) {
 		Optional<User> obj =  repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
